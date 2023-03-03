32b4a30f7ebcb4bb3ad91d325ea3364c8ff5cfa4
shankhsuri <shankhsuri@gmail.com>
2023-03-02 22:38:03 -0500

    fix

--- dataset.py
+++ dataset.py
@@ -5,40 +5,33 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data.sampler import *
 from torch.utils.data import Dataset
+from torchvision.transforms import InterpolationMode
 from torchvision import transforms, datasets
-import torchvision
 import numpy as np
 import torchvision.models as models
 
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import ImageFolder
+
 # Get the transforms
-def load_datasets():
-
-    # Transforms for the image.
+def load_datasets(batch_size=32, num_workers=4):
     transform = transforms.Compose([
-                        transforms.Grayscale(3),
-                        transforms.Resize(224,224),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5,), (0.5,))
-                ])
+        transforms.Resize((224, 224), interpolation=InterpolationMode.BILINEAR),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
-    # Define the image folder for each of the data set types
-    trainset = torchvision.datasets.ImageFolder(
-        root= "data/" + 'train',
-        transform=transform
-    )
-    validset = torchvision.datasets.ImageFolder(
-        root="data/" + 'val',
-        transform=transform
-    )
-    testset = torchvision.datasets.ImageFolder(
-        root="data/" + 'test',
-        transform=transform
-    )
+    dataset = ImageFolder('data', transform=transform)
+    num_samples = len(dataset)
+    train_size = int(num_samples * 0.8)
+    val_size = (num_samples - train_size) // 2
+    test_size = num_samples - train_size - val_size
 
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
-    # Define indexes and get the subset random sample of each.
-    train_dataloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
-    valid_dataloader = torch.utils.data.DataLoader(validset, batch_size=128, shuffle=True)
-    test_dataloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True)
-        
-    return train_dataloader, valid_dataloader, test_dataloader
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    valid_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, valid_loader, test_loader
